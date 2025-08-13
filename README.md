71bb30772694d538d5cfe764172269b5a0b1c163
Agustin <agus_ph_2@outlook.com>
2025-08-13 20:08:38 -0300

    Estructura inicial con README y carpeta ejercicios_python

---
+++ README.md
@@ -0,0 +1,7 @@
+# desarrollo_de_software
+
+Repositorio para ejercicios de la materia Desarrollo de Software.
+
+## Contenido
+
+- Carpeta `ejercicios_python` con ejercicios en Python.
---
+++ ejercicios_python/ejercicio_1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Ejercicio 1: Escribir un programa en Python que imprima el mensaje “¡Hola Mundo!”.
+"""
+
+def main():
+    print("Hola, este es el ejercicio 1")
+
+if __name__ == "__main__":
+    main()
